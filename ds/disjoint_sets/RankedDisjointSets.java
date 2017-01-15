5415c98f98ee23536a972cceb2c1a9224911e46a
Khiem Pham <Khiem Pham>
2017-01-14 20:22:48 -0800

    add data structure

--- ds/disjoint_sets/RankedDisjointSets.java
+++ ds/disjoint_sets/RankedDisjointSets.java
@@ -1,13 +1,26 @@
 package disjoint_sets;
 
 /**
+ * A simple union-by-rank-with-path-compression implementation of disjoint sets data structure.
+ * array index represents the name of node
+ * negative array content: the node is a parent, its absolute value the size of the subtree
+ * positive array content: the parent of this node
+ * every array content starts with -1: every node initially is a parent with rank 1
  * Created by Khiem on 1/14/2017.
  */
 public class RankedDisjointSets extends DisjointSets {
     public RankedDisjointSets(int numElements) {
         super(numElements);
+        for (int i = 0; i < numElements; i++) {
+            parents[i] = -1;
+        }
     }
 
+    /**
+     * union-by-rank: each node initially has rank 1.Each time 2 trees are concatenated,
+     * the root of the tree with smaller rank will be linked to the root of the bigger one
+     * rank is not necessarily height as path compression with alter heights online
+     */
     public void union(int x, int y) {
         x = find(x);
         y = find(y);
@@ -22,6 +35,13 @@ public class RankedDisjointSets extends DisjointSets {
 
     }
 
+    /**
+     * path-compression: Each time 2 trees are joined, the nodes that are traversed in the smaller tree
+     * will be linked to the root of the larger tree. This makes the tree less deep, allowing faster
+     * access in next find. It has been shown that the amortized find is practically below 5 (though unbounded)
+     * @param x
+     * @return
+     */
     public int find(int x) {
         if (parents[x] < 0) {
             return x;
---
+++ ds/segmenttree/SegmentTree.java
@@ -0,0 +1,61 @@
+/**
+ * Created by Khiem on 10/14/2016.
+ */
+public class SegmentTree {
+    private int[] st;
+    private int[] a; //original array
+
+    public SegmentTree(int[] a) {
+        this.a  = a;
+        st = new int[a.length*4];
+        build(1, 0, a.length-1);
+    }
+
+    private int left(int p) {
+        return (p << 1);
+    }
+
+    private int right(int p) {
+        return (p << 1) + 1;
+    }
+
+    public void build(int p, int l, int r) {
+        if (l == r) {
+            st[p] = l;
+        } else {
+            build(left(p), l, (l + r) / 2);
+            build(right(p), (l + r) / 2 + 1, r);
+            int p1 = st[left(p)], p2 = st[right(p)];
+            st[p] = (a[p1] <= a[2]) ? p1 : p2;
+        }
+    }
+
+    public int rmq(int i, int j) {
+        return rmq(1, 0, a.length - 1, i, j);
+
+    }
+    public int rmq(int p, int l, int r, int i, int j) {
+        if (i > r || j < l) {
+            return -1;
+        }
+        if (i <= l && j >= r) {
+            return st[p];
+        }
+        int p1 = rmq(left(p), l, (l + r) / 2, i, j);
+        int p2 = rmq(right(p), (l + r) / 2 + 1, r, i, j);
+        if (p1 == -1) {
+            return p2;
+        }
+        if (p2 == -1) {
+            return p1;
+        }
+        return (a[p1] <= a[p2]) ? p1 : p2;
+    }
+
+
+    public static void main(String[] args) {
+        int[] a = {18, 17, 13, 19, 15, 11, 20};
+        SegmentTree st  = new SegmentTree(a);
+        System.out.println(st.rmq(4,6));
+    }
+}
