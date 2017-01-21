e9d2c5df60c86c7085da31746a8865435597bfd9
Khiem Pham <duckhiem95@yahoo.com>
2017-01-20 17:30:03 -0800

    knn

--- k_nearest_neighbors/knn/src/NDPoint.java
+++ k_nearest_neighbors/knn/src/NDPoint.java
@@ -1,7 +1,29 @@
-package PACKAGE_NAME;
-
 /**
  * Created by Khiem on 1/19/2017.
  */
 public class NDPoint {
+    double[] co;
+    int dim;
+    int value;
+
+    public NDPoint(double... args) {
+        dim = args.length;
+        for (int i = 0; i < dim; i++) {
+            co[i] = args[i];
+        }
+    }
+
+    public int setValue(int v) {
+        value = v;
+    }
+
+    public double distTo(NDPoint other) {
+
+        double sum = 0;
+        for (int i = 0; i < dim; i++) {
+            sum += Math.pow(co[i] - other.co[i], 2);
+        }
+        return Math.sqrt(sum);
+    }
+
 }
--- k_nearest_neighbors/knn/src/knn.java
+++ k_nearest_neighbors/knn/src/knn.java
@@ -1,7 +1,6 @@
-package PACKAGE_NAME;
-
 /**
  * Created by Khiem on 1/19/2017.
  */
 public class knn {
+
 }
